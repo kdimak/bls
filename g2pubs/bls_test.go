8651ea46098a56bc20edd196fbda0e346079456f
Dmitriy Kinoshenko <dkinoshenko@gmail.com>
2020-10-05 18:54:48 +0300

    test: verify VC proof containing of 25 messages.

--- g2pubs/bls_test.go
+++ g2pubs/bls_test.go
@@ -196,6 +196,80 @@ func TestVerifySignature(t *testing.T) {
 
 }
 
+func TestVerifyVCProof(t *testing.T) {
+	pkBase64 := "h/rkcTKXXzRbOPr9UxSfegCbid2U/cVNXQUaKeGF7UhwrMJFP70uMH0VQ9+3+/2zDPAAjflsdeLkOXW3+ShktLxuPy8UlXSNgKNmkfb+rrj+FRwbs13pv/WsIf+eV66+"
+	pkBytes, err := base64.RawStdEncoding.DecodeString(pkBase64)
+	require.NoError(t, err)
+	require.Len(t, pkBytes, 96)
+
+	var pkBytesArr [96]byte
+	copy(pkBytesArr[:], pkBytes[:96])
+
+	publicKey, err := g2pubs.DeserializePublicKey(pkBytesArr)
+	require.NoError(t, err)
+	require.NotNil(t, publicKey)
+
+	sigBase64 := "g0j/GDxXkBTazGC18VpR/jfqebByufhKWtzMXI9OrxFQlZoGRmDreAv8Rl+lhNQzPjA1Yn9WjcuucOF6f2VMkFUFB2FUzYfegJ77Q+X/JIxHpy1MSQFHi4dtPtAxJpmrJ1Y65aHdkSI8icmnl6gAIA=="
+	sigBytes, err := base64.StdEncoding.DecodeString(sigBase64)
+	require.NoError(t, err)
+	require.Len(t, sigBytes, 112)
+
+	var sigBytesArr [48]byte
+	copy(sigBytesArr[:], sigBytes[:48])
+
+	signature, err := g2pubs.DeserializeSignature(sigBytesArr)
+	require.NoError(t, err)
+	require.NotNil(t, signature)
+
+	e := parseFr(sigBytes[48 : 48+32])
+	require.NotNil(t, e)
+
+	s := parseFr(sigBytes[48+32:])
+	require.NotNil(t, s)
+	messages := []string{
+		`_:c14n0 <http://purl.org/dc/terms/created> "2020-10-05T15:37:27Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .`,
+		`_:c14n0 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3c-ccg.github.io/ldp-bbs2020/context/v1#BbsBlsSignature2020> .`,
+		`_:c14n0 <https://w3id.org/security#proofPurpose> <https://w3id.org/security#assertionMethod> .`,
+		`_:c14n0 <https://w3id.org/security#verificationMethod> <did:example:489398593#test> .`,
+		`<did:example:b34ca6cd37bbf23> <http://schema.org/birthDate> "1958-07-17"^^<http://www.w3.org/2001/XMLSchema#dateTime> .`,
+		`<did:example:b34ca6cd37bbf23> <http://schema.org/familyName> "SMITH" .`,
+		`<did:example:b34ca6cd37bbf23> <http://schema.org/gender> "Male" .`,
+		`<did:example:b34ca6cd37bbf23> <http://schema.org/givenName> "JOHN" .`,
+		`<did:example:b34ca6cd37bbf23> <http://schema.org/image> <data:image/png;base64,iVBORw0KGgokJggg==> .`,
+		`<did:example:b34ca6cd37bbf23> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Person> .`,
+		`<did:example:b34ca6cd37bbf23> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/citizenship#PermanentResident> .`,
+		`<did:example:b34ca6cd37bbf23> <https://w3id.org/citizenship#birthCountry> "Bahamas" .`,
+		`<did:example:b34ca6cd37bbf23> <https://w3id.org/citizenship#commuterClassification> "C1" .`,
+		`<did:example:b34ca6cd37bbf23> <https://w3id.org/citizenship#lprCategory> "C09" .`,
+		`<did:example:b34ca6cd37bbf23> <https://w3id.org/citizenship#lprNumber> "999-999-999" .`,
+		`<did:example:b34ca6cd37bbf23> <https://w3id.org/citizenship#residentSince> "2015-01-01"^^<http://www.w3.org/2001/XMLSchema#dateTime> .`,
+		`<https://issuer.oidp.uscis.gov/credentials/83627465> <http://schema.org/description> "Government of Example Permanent Resident Card." .`,
+		`<https://issuer.oidp.uscis.gov/credentials/83627465> <http://schema.org/identifier> "83627465" .`,
+		`<https://issuer.oidp.uscis.gov/credentials/83627465> <http://schema.org/name> "Permanent Resident Card" .`,
+		`<https://issuer.oidp.uscis.gov/credentials/83627465> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/citizenship#PermanentResidentCard> .`,
+		`<https://issuer.oidp.uscis.gov/credentials/83627465> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://www.w3.org/2018/credentials#VerifiableCredential> .`,
+		`<https://issuer.oidp.uscis.gov/credentials/83627465> <https://www.w3.org/2018/credentials#credentialSubject> <did:example:b34ca6cd37bbf23> .`,
+		`<https://issuer.oidp.uscis.gov/credentials/83627465> <https://www.w3.org/2018/credentials#expirationDate> "2029-12-03T12:19:52Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .`,
+		`<https://issuer.oidp.uscis.gov/credentials/83627465> <https://www.w3.org/2018/credentials#issuanceDate> "2019-12-03T12:19:52Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .`,
+		`<https://issuer.oidp.uscis.gov/credentials/83627465> <https://www.w3.org/2018/credentials#issuer> <did:example:489398593> .`,
+	}
+
+	messagesFr := make([]*bls.FR, len(messages))
+	for i := range messages {
+		messagesFr[i] = messageToFr([]byte(messages[i]))
+	}
+
+	p1 := signature.GetPoint().ToAffine()
+	require.NotNil(t, p1)
+
+	q1 := bls.G2ProjectiveOne
+	q1 = Mul(q1, *e.ToRepr())
+	q1 = q1.Add(publicKey.GetPoint())
+	p2 := getB(s, messagesFr, publicKey)
+
+	require.True(t, CompareTwoPairings(p1.ToProjective(), q1, p2.ToProjective(), bls.G2ProjectiveOne))
+}
+
 func calcData(key *g2pubs.PublicKey, messagesCount int) []byte {
 	keyBytes := key.GetPoint().ToAffine().SerializeBytes()
 	data := keyBytes[:]
